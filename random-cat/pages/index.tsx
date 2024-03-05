0071bd84c248176f18cc201d6620abead6977595
Tsukasa-Nakajo <tsukasa_nakajo@nuco.co.jp>
2024-03-05 10:40:52 +0900

    add like-button

--- random-cat/pages/index.tsx
+++ random-cat/pages/index.tsx
@@ -1,53 +1,55 @@
-import {GetServerSideProps, NextPage} from "next";
-import {useState} from "react";
-import styles from "./index.module.css"
+import { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
+import styles from "./index.module.css";
 
+// getServerSidePropsから渡されるpropsの型
 type Props = {
   initialImageUrl: string;
 };
 
-const IndexPage: NextPage<Props> = ({initialImageUrl}) =>{
-  const[imageUrl,setImageUrl] =  useState(initialImageUrl);
-  const[loading,setLoading] = useState(false);
-
-
-const handleClick = async () =>{
-  setLoading(true);
-  const newImage = await fetchImage();
-  setImageUrl(newImage.url)
-  setLoading(false);
-};
-return(
-  <div className={styles.page}>
-    <button onClick={handleClick} className={styles.button}>
-      他のニャンコも見る
+// ページコンポーネント関数にpropsを受け取る引数を追加する
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+  const [imageUrl, setImageUrl] = useState(initialImageUrl); // 初期値を渡す
+  const [loading, setLoading] = useState(false); // 初期状態はfalseにしておく
+  // useEffect(() => {
+  //   fetchImage().then((newImage) => {
+  //     setImageUrl(newImage.url);
+  //     setLoading(false);
+  //   });
+  // }, []);
+  const handleClick = async () => {
+    setLoading(true);
+    const newImage = await fetchImage();
+    setImageUrl(newImage.url);
+    setLoading(false);
+  };
+  return (
+    <div className={styles.page}>
+      <button onClick={handleClick} className={styles.button}>
+        他のにゃんこも見る
       </button>
-    <div className={styles.frame}>
-      {loading || <img src={imageUrl} className={styles.img} />}
+      <div className={styles.frame}>
+        {loading || <img src={imageUrl} className={styles.img} />}
       </div>
-  </div>
-);
-
-
-  return <div>{loading || <img src={imageUrl} />}</div>;
-
+    </div>
+  );
 };
 export default IndexPage;
 
-export const getServerSideProps:GetServerSideProps<Props> = async () =>{
+// サーバーサイドで実行する処理
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
   const image = await fetchImage();
-  return{
-    props:{
+  return {
+    props: {
       initialImageUrl: image.url,
     },
   };
 };
 
 type Image = {
-  url:string;
+  url: string;
 };
-
-const fetchImage = async ():Promise<Image> => {
+const fetchImage = async (): Promise<Image> => {
   const res = await fetch("https://api.thecatapi.com/v1/images/search");
   const images = await res.json();
   console.log(images);
